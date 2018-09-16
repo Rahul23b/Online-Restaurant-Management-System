a01b9e3bbef29fe1ba4485915135c55d6c54de19
Celin Christy Anandhi Paul Jeyaraj <43323148+christyjeyaraj@users.noreply.github.com>
2018-09-16 17:51:21 -0400

    Add files via upload

---
+++ env-setup-for-tomcat_backup.bat
@@ -0,0 +1,11 @@
+set JAVA_HOME=C:\jdk1.7
+
+set PATH="C:\jdk1.7\bin";%PATH%
+
+set CLASSPATH=.;C:\apache-tomcat-7.0.34\lib\servlet-api.jar;C:\apache-tomcat-7.0.34\lib\jsp-api.jar;C:\apache-tomcat-7.0.34\lib\el-api.jar;C:\csj\Assignment\WEB-INF\lib\bson-3.0.2.jar;C:\csj\Assignment\WEB-INF\lib\mongodb-driver-3.0.2.jar;C:\csj\Assignment\WEB-INF\lib\mongodb-driver-core-3.0.2.jar;C:\csj\Assignment\WEB-INF\lib\standard.jar;C:\csj\Assignment\WEB-INF\lib\jstl.jar
+
+set ANT_HOME=C:\apache-tomcat-7.0.34
+
+set TOMCAT_HOME=C:\apache-tomcat-7.0.34
+
+set CATALINA_HOME=C:\apache-tomcat-7.0.34
